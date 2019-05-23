31e9ad7f7a5b2431bfbd7da0c5aefe3562b61957
LuBo <35888279+lubocsu@users.noreply.github.com>
2019-05-23 14:38:02 +0800

    Add files via upload

---
+++ 01编程入门/项目3/entertaiment_center.py
@@ -0,0 +1,7 @@
+import media
+forrest_fump = media.Movie("Forrest_Gump",
+              "In 1981, Forrest Gump recounts his life story to strangers who sit next to him on a bench at a bus stop "
+              "in Savannah, Georgia.","https://en.wikipedia.org/wiki/Forrest_Gump#/media/File:Forrest_Gump_poster.jpg",
+               "http://www.iqiyi.com/v_19rrk3u264.html")
+
+forrest_fump.play_trailer()
